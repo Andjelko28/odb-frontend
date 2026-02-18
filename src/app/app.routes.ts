80894fe837ce68edd0b3bc17313a85937efb8da8
Andjelko28 <andjelkofustic99@gmail.com>
2026-02-18 19:31:57 +0100

    Created home, register and login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/pages/home/home';
+import { Register } from './components/pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'register', component: Register },
+];
