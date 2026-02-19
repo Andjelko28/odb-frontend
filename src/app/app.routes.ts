67926247e25c282d3562715614e79344f435da3c
Andjelko28 <andjelkofustic99@gmail.com>
2026-02-19 13:39:11 +0100

    Styling for login and register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
 import { Register } from './components/pages/register/register';
+import { Login } from './components/pages/login/login';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'register', component: Register },
+  {
+    path: 'login',
+    component: Login,
+  },
 ];
