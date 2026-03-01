6d9b4bc8c8af63ba356578fa4b766d4d0e6bc85c
Andjelko28 <andjelkofustic99@gmail.com>
2026-03-01 15:27:08 +0100

    Login errors handling

--- src/app/components/pages/login/login.ts
+++ src/app/components/pages/login/login.ts
@@ -1,11 +1,57 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  loginForm: FormGroup;
+  loading = false;
+  error?: string;
 
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  submit(): void {
+    // if (this.loginForm.invalid) {
+    //   this.loginForm.markAllAsTouched();
+    //   return;
+    // }
+    const { email, password } = this.loginForm.value;
+    this.loading = true;
+
+    this.authService.login(email, password).subscribe({
+      next: () => {
+        this.loading = false;
+        this.loginForm.reset();
+      },
+      error: (err) => {
+        // this.error = err.error?.message || 'Logging in failed. Please try again.';
+        this.loading = false;
+        if (err.status === 401) {
+          const passwordControl = this.loginForm.get('password');
+          const emailControl = this.loginForm.get('email');
+          emailControl?.setErrors({ invalidCredentials: true });
+          emailControl?.markAsTouched();
+          passwordControl?.setErrors({ invalidCredentials: true });
+          passwordControl?.markAsTouched();
+        } else {
+          this.loginForm.setErrors({ serverError: true });
+        }
+      },
+    });
+  }
 }
